718dfa59a0acb24de0e03f69fbec1258fc1ad71e
lorenzo <lorenzocav97@gmail.com>
2024-02-05 19:55:46 +0100

    created docker with db

--- db/init.sql
+++ db/init.sql
@@ -1,20 +1,11 @@
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
+CREATE TABLE IF NOT EXISTS account (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL
 );
 
-
--- CREATE TABLE IF NOT EXISTS user (
---     id serial PRIMARY KEY,
---     name varchar(255) NOT NULL
--- ),
-
--- CREATE TABLE IF NOT EXISTS photo (
---     id serial PRIMARY KEY,
---     photo_path varchar(255) NOT NULL,
---     date_upload bigint NOT NULL,
---     user_id serial REFERENCES cities(id) NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS photo (
+    id serial PRIMARY KEY,
+    photo_path varchar(255) NOT NULL,
+    date_upload bigint NOT NULL,
+    user_id serial REFERENCES account(id) NOT NULL
+);
